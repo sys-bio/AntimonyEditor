cee76c9dd4183d147a377605bf38dc1ed41f5017
evaliu2002 <evaxinerliu@gmail.com>
2023-08-17 22:24:30 -0700

    indexeddb implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import FileExplorer from './FileUploader';
 import CodeEditor from './AntimonyEditor';
+import { openDB, DBSchema, IDBPDatabase } from 'idb';
+
+interface MyDB extends DBSchema {
+  files: {
+    key: string;
+    value: { name: string; content: string };
+  };
+}
 
 const sampleAntimonyModel = `
 // Sample Antimony Model
@@ -22,14 +30,32 @@ const App: React.FC = () => {
   const [uploadedFiles, setUploadedFiles] = useState<{ name: string; content: string }[]>([]);
   const [selectedFileContent, setSelectedFileContent] = useState<string>(sampleAntimonyModel);
 
-  const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+  useEffect(() => {
+    openDB<MyDB>('fileDB', 1, {
+      upgrade(db) {
+        if (!db.objectStoreNames.contains('files')) {
+          db.createObjectStore('files', { keyPath: 'name' });
+        }
+      },
+    }).then(db => {
+      db.getAll('files').then(files => {
+        setUploadedFiles(files);
+      });
+    });
+  }, []);
+
+  const handleFileUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
     const files = event.target.files;
     if (files) {
-      Array.from(files).forEach(file => {
+      const db = await openDB<MyDB>('fileDB', 1);
+
+      Array.from(files).forEach(async file => {
         const reader = new FileReader();
         reader.readAsText(file);
-        reader.onload = () => {
-          setUploadedFiles(prevFiles => [...prevFiles, { name: file.name, content: reader.result as string }]);
+        reader.onload = async () => {
+          const fileData = { name: file.name, content: reader.result as string };
+          await db.put('files', fileData);
+          setUploadedFiles(prevFiles => [...prevFiles, fileData]);
         };
       });
     }
