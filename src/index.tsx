78c6bd3fc3cfef5cd1c1544d6e8d9c56936b2bf6
evaliu2002 <evaxinerliu@gmail.com>
2023-08-18 21:03:56 -0700

    web worker warning removed

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,28 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// index.ts
+
+import * as monaco from 'monaco-editor';
+import { customLanguageWorker } from './languages/CustomLanguageWorker';
+
+// Define the getWorker function
+// Dummy web worker function for now, will build more upon when parser and syntax highlighting is created
+const getWorker = (_moduleId: string, label: string): Worker => {
+  return new Worker(
+    URL.createObjectURL(
+      new Blob([`self.onmessage = ${customLanguageWorker}`], {
+        type: 'application/javascript',
+      })
+    )
+  );
+};
+
+// Set the getWorker function in MonacoEnvironment
+(window as any).MonacoEnvironment = {
+  getWorker: getWorker,
+};
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
