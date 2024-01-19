813c59f58acc68239a967e69d804eba0c7658008
evaliu2002 <evaxinerliu@gmail.com>
2024-01-18 20:02:10 -0800

    fixed libant local error?

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './testing/reportWebVitals';
+import reportWebVitals from './reportWebVitals';
 import * as monaco from 'monaco-editor';
 import { customLanguageWorker } from './languages/antlr/CustomLanguageWorker';
 
