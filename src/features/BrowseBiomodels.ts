d70ee3feddab39ba720910fb05126ebe2eccbfb9
Anish Konanki <anishkonanki@gmail.com>
2024-02-13 14:49:14 -0800

    Cache search update

--- src/features/BrowseBiomodels.ts
+++ src/features/BrowseBiomodels.ts
@@ -1,40 +1,78 @@
+import cache from "./test_cache.json";
+/**
+* Interface for the biomodels API response
+* @interface Model
+* @property {string} name - The name of the model
+* @property {string} url - The URL of the model
+* @property {string} id - The ID of the model
+*/
 interface Model {
     name: string;
     url: string;
     id: string;
 }
 
+interface CachedData {
+  [key: string]: {
+      name: string;
+      authors: string[];
+      url: string;
+      model_id: string;
+      title: string;
+      synopsis: string;
+  };
+}
+
+/**
+ * Interface for models returned by the biomodels API
+ * @interface Models
+ * @property {Map<String, Model>} models - The models returned by the biomodels API
+ */
 interface Models {
     models: Map<String, Model>;
 }
-
+// API URL for redirecting to the biomodels API
 const corsProxyUrl = "https://api.allorigins.win/raw?url=";
 
+// The cache of models retrieved from a JSON file
+const cachedData: CachedData = cache;
+
+/**
+ * Function to search for models using the biomodels API
+ * @param {KeyboardEvent} search - The search event
+ * @returns {Promise<Models>} - A promise containing the models returned by the biomodels API
+ */
 export async function searchModels(search: KeyboardEvent) {
     try {
+        // Get the search query
         const queryText = (search.target as HTMLInputElement).value.trim();
-        const response = await fetch(corsProxyUrl + `https://www.ebi.ac.uk/biomodels/search?query=${queryText}%26numResults=25%26format=json`)
-        if (response.ok) {
-            const results = await response.json();
-            const models: Models = { models: new Map() };
-            results.models.forEach((model: any) => {
-                if (model.id in models.models) return;
-                models.models.set(model.id, {
-                    name: model.name,
-                    url: model.url,
-                    id: model.id
-                });
+        console.log(queryText);
+        const models: Models = { models: new Map() };
+        for (const id in cachedData) {
+          const modelData = cachedData[id];
+          if (Object.values(modelData).some(value => 
+            typeof value === "string" && value.toLowerCase().includes(queryText.toLowerCase()))) {
+            models.models.set(id, {
+              name: modelData.name,
+              url: modelData.url,
+              id: modelData.model_id
             });
-            return models;
-        } else {
-            throw new Error('Error when fetching search results');
+          }
         }
+        console.log(models);
+        return models;
     } catch (error) {
+        // If there is an error, throw it
         console.log(error);
         throw error;
     }
 }
 
+/**
+ * Function to get a model from the biomodels API
+ * @param {string} modelId - The ID of the model to get
+ * @returns {Promise<string>} - A promise containing the model
+ */
 export async function getModel(modelId: string) {
     try {
         const response = await fetch(corsProxyUrl + `https://www.ebi.ac.uk/biomodels/model/download/${modelId}?filename=${modelId}_url.xml`);
@@ -51,6 +89,12 @@ export async function getModel(modelId: string) {
     }
 }
 
+/**
+ * Function to display the models in the dropdown
+ * @param {React.Dispatch<React.SetStateAction<boolean>>} setLoading - The setLoading function from the parent component
+ * @param {React.Dispatch<React.SetStateAction<string | null>>} setChosenModel - The setChosenModel function from the parent component
+ * @returns {void}
+ */
 export function getBiomodels(setLoading: React.Dispatch<React.SetStateAction<boolean>>, setChosenModel: React.Dispatch<React.SetStateAction<string | null>>) {
   const biomodelBrowse = document.getElementById('biomodel-browse') as HTMLInputElement;
   const dropdown = document.getElementById('dropdown');
@@ -59,7 +103,7 @@ export function getBiomodels(setLoading: React.Dispatch<React.SetStateAction<boo
 
   biomodelBrowse.addEventListener('keyup', async (val) => {
     const biomodel = val;
-    if ((val.target as HTMLInputElement).value.length < 3) {
+    if ((val.target as HTMLInputElement).value.length < 2) {
       dropdown!.innerHTML = "";
       return;
     }
