83c8dfddc088f6e1552d5dba61adcf1d3a4f8e9d
qndan <80284796+qndan@users.noreply.github.com>
2025-04-08 23:36:38 +0000

    improve FileExplorer rename test to be less implementation-defined

--- src/__tests__/FileExplorer.tests.tsx
+++ src/__tests__/FileExplorer.tests.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { render, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import FileExplorer from "../components/file-explorer/FileExplorer";
 import { act } from "react-dom/test-utils";
@@ -7,6 +8,7 @@ import { act } from "react-dom/test-utils";
 test("when renaming file, pressing enter should finish the rename", async () => {
     const initialFileName = "hi click me"
     const newFileName = "hi click me 2"
+    const typeThis = " 2{Enter}"; // This is what they type to go from the initial file name to the new file name
 
     // We have to create a container for the file explorer so we can use `useState` to mock the files.
     const FileExplorerMockContainer = () => {
@@ -38,27 +40,18 @@ test("when renaming file, pressing enter should finish the rename", async () => 
 
     /// now click rename
     const renameButton = await explorer.findByText("Rename")
-    expect(renameButton).toBeInstanceOf(HTMLLIElement);
+    expect(renameButton).toBeInTheDocument();
     await fireEvent.click(renameButton);
 
     // check that it is now renaming
     const fileInput = await explorer.findByDisplayValue(initialFileName);
     expect(fileInput).toBeInstanceOf(HTMLInputElement);
+    expect(fileInput).toHaveFocus();
 
-    // rename it
-    await act(async () => {
-        await fireEvent.change(fileInput, {
-            target: {
-                value: newFileName,
-            }
-        })
-        await fireEvent.keyDown(fileInput, {
-            key: "Enter",
-        });
-    });
+    // type into the box the new name
+    userEvent.keyboard(typeThis);
 
     // the file should now be renamed
     const fileButton2 = await explorer.findByText(newFileName);
     expect(fileButton2).toBeInstanceOf(HTMLButtonElement);
-    expect(fileButton2.textContent).toBe(newFileName);
 })
